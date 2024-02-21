40668adfaaca12937444dc810371e70e4efa27a0
pizdinad <1@pizdinad.ru>
2024-02-21 11:37:28 +0300

    geo

--- js/MainClass.js
+++ js/MainClass.js
@@ -256,6 +256,35 @@ initFooterEl();
 
 if ("geolocation" in navigator) {
   console.log("местоположение доступно ");
+
+  const geo = navigator.geolocation;
+
+
+
+  var options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0,
+  };
+
+  function success(pos) {
+    var crd = pos.coords;
+
+    console.log("Ваше текущее местоположение:");
+    console.log(`Широта: ${crd.latitude}`);
+    console.log(`Долгота: ${crd.longitude}`);
+    console.log(`Плюс-минус ${crd.accuracy} метров.`);
+  }
+
+  function error(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+  }
+
+  geo.getCurrentPosition(success, error, options);
+
+
+
+
 } else {
   console.log("местоположение НЕ доступно");
 }
