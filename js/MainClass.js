940b4ad1c9df2c5fa8845a6450798fd42ad9bad6
pizdinad <1@pizdinad.ru>
2024-02-21 10:45:49 +0300

    clientIP-1

--- js/MainClass.js
+++ js/MainClass.js
@@ -226,9 +226,9 @@ async function initYourIP() {
   const { clientIP } = json;
 
 
-  console.log("clientIP: ", clientIP);
+  // console.log("clientIP: ", clientIP); // 
 
-
+  your_ip_el.textContent = `Your ip: ${clientIP}`;
 
 
 
